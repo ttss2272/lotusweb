ad7a0b8f2f3998445a42ccf8b729ff66a8ed57e1
ttss2272 <admin@teratech-solutions.com>
2015-09-16 18:34:41 +0530

    PriTesh

--- src/MedicalShopWeb/MedicalShopWeb/MasterPages/MasterPage.Master.designer.cs
+++ src/MedicalShopWeb/MedicalShopWeb/MasterPages/MasterPage.Master.designer.cs
@@ -31,12 +31,21 @@ namespace MedicalShopWeb.MasterPages {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// CntPlcLeft control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder CntPlcLeft;
+        
+        /// <summary>
+        /// CntPlcCenter control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder CntPlcCenter;
     }
 }
