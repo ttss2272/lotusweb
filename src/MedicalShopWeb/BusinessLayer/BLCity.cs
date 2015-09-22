953a2a5488c0b4a2afb31b47590ef57b70990d11
ttss2272 <admin@teratech-solutions.com>
2015-09-22 13:53:55 +0530

    PriTesh

---
+++ src/MedicalShopWeb/BusinessLayer/BLCity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BLCity
+    {
+        DLCity objCity = new DLCity();
+        public string SaveCity(int CityID, string CityName, int StateID, int UpdatedByUserID, int IsActive)
+        {
+            string Result = objCity.SaveCity(CityID, CityName, StateID, UpdatedByUserID, IsActive);
+            return Result;
+        }
+
+        public DataSet GetCity(int CityID, int IsActive)
+        {
+            DataSet dsCity = objCity.GetCity(CityID, IsActive);
+            return dsCity;
+        }
+
+        public DataSet BindCity(int StateID)
+        {
+            DataSet dsCity = objCity.BindCity(StateID);
+            return dsCity;
+        }
+    }
+}
---
+++ src/MedicalShopWeb/BusinessLayer/BLState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BLState
+    {
+        DLState objState = new DLState();
+
+        public string SaveState(int StateID, string StateName, int CountryID, int UpdatedByUserID, int IsActive)
+        {
+            string Result = objState.SaveState(StateID, StateName, CountryID, UpdatedByUserID, IsActive);
+            return Result;
+        }
+
+        public DataSet GetState(int StateID, int IsActive)
+        {
+            DataSet dsState = objState.GetState(StateID, IsActive);
+                return dsState;
+        }
+
+        public DataSet BindState(int CountryID)
+        {
+            DataSet dsState = objState.BindState(CountryID);
+            return dsState;
+        }
+    }
+}
