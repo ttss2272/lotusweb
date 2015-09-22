f029898cae5bfabffe8ca7a9658e4e9c1a5f7a82
ttss2272 <admin@teratech-solutions.com>
2015-09-22 20:31:19 +0530

    PriTesh

--- src/MedicalShopWeb/DataLayer/DLMedicalShop.cs
+++ src/MedicalShopWeb/DataLayer/DLMedicalShop.cs
@@ -29,5 +29,31 @@ namespace DataLayer
             return dsShopType;
  
         }
+
+        public string SaveMedicalShop(int MedicalShopID, string OwnerName, string ContactNo, int CityID, string Area, string ShopName, int ShopTypeID, decimal OpeningBalance, int UpdatedByUserID, int IsActive)
+        {
+            string Result =null;
+
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("SaveCounty_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@MedicalShopID", MedicalShopID);
+            cmd.Parameters.AddWithValue("@OwnerName", OwnerName);
+            cmd.Parameters.AddWithValue("@ContactNo", ContactNo);
+            cmd.Parameters.AddWithValue("@CityID", CityID);
+            cmd.Parameters.AddWithValue("@Area", Area);
+            cmd.Parameters.AddWithValue("@ShopName", ShopName);
+            cmd.Parameters.AddWithValue("@ShopTypeID", ShopTypeID);
+            cmd.Parameters.AddWithValue("@OpeningBalance", OpeningBalance);
+            cmd.Parameters.AddWithValue("@UpdatedByUserID", UpdatedByUserID);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+
+            con.Open();
+            Result = cmd.ExecuteScalar().ToString();
+            con.Close();
+            return Result;
+            
+        }
     }
 }
