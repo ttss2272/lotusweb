b8f56e636eea054ce2dccd00bf3c801ed8dae1af
ttss2272 <admin@teratech-solutions.com>
2015-09-22 20:32:12 +0530

    PriTesh

--- src/MedicalShopWeb/BusinessLayer/BLMedicalShop.cs
+++ src/MedicalShopWeb/BusinessLayer/BLMedicalShop.cs
@@ -16,5 +16,12 @@ namespace BusinessLayer
             DataSet dsShopType = objMedicalShop.GetShopType();
             return dsShopType;
         }
+
+        public string SaveMedicalShop(int MedicalShopID, string OwnerName, string ContactNo, int CityID, string Area, string ShopName, int ShopTypeID, decimal OpeningBalance, int UpdatedByUserID, int IsActive)
+        {
+            string Result = objMedicalShop.SaveMedicalShop(MedicalShopID, OwnerName, ContactNo, CityID, Area, ShopName, ShopTypeID, OpeningBalance, UpdatedByUserID, IsActive);
+            return Result;
+
+        }
     }
 }
