00f4a47cc37242b9d18269099d8af77a3e406b1f
ttss2272 <admin@teratech-solutions.com>
2015-09-22 13:54:20 +0530

    PriTesh

---
+++ src/MedicalShopWeb/DataLayer/DLCity.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DLCity
+    {
+        SqlConnection con = new SqlConnection();
+        DBConnection conn = new DBConnection();
+
+
+        public string SaveCity(int CityID, string CityName, int StateID, int UpdatedByUserID, int IsActive)
+        {
+            string Result = null;
+            con = conn.GetConnection();
+
+            SqlCommand cmd = new SqlCommand("SaveCity_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@CityID", CityID);
+            cmd.Parameters.AddWithValue("@CityName", CityName);
+            cmd.Parameters.AddWithValue("@StateID", StateID);
+            cmd.Parameters.AddWithValue("@UpdatedByUserID", UpdatedByUserID);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+
+            con.Open();
+            Result = cmd.ExecuteScalar().ToString();
+            con.Close();
+            return Result;
+        }
+
+        public DataSet GetCity(int CityID, int IsActive)
+        {
+            DataSet dsCity = new DataSet();
+
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("GetCity_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@CityID", CityID);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+            con.Open();
+
+            SqlDataAdapter daGetCountryData = new SqlDataAdapter(cmd);
+            dsCity = new DataSet();
+            daGetCountryData.Fill(dsCity);
+            con.Close();
+            return dsCity;
+        }
+
+        public DataSet BindCity(int StateID)
+        {
+            DataSet dsCity = new DataSet();
+
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("BindCity_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@StateID", StateID);
+            
+            con.Open();
+
+            SqlDataAdapter daGetCountryData = new SqlDataAdapter(cmd);
+            dsCity = new DataSet();
+            daGetCountryData.Fill(dsCity);
+            con.Close();
+            return dsCity;
+ 
+        }
+    }
+}
---
+++ src/MedicalShopWeb/DataLayer/DLState.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DLState
+    {
+        SqlConnection con = new SqlConnection();
+        DBConnection conn = new DBConnection();
+
+        public string SaveState(int StateID, string StateName, int CountryID, int UpdatedByUserID, int IsActive)
+        {
+            string Result = null;
+            con = conn.GetConnection();
+
+            SqlCommand cmd = new SqlCommand("SaveState_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@StateID", StateID);
+            cmd.Parameters.AddWithValue("@StateName", StateName);
+            cmd.Parameters.AddWithValue("@CountryID", CountryID);
+            cmd.Parameters.AddWithValue("@UpdatedByUserID", UpdatedByUserID);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+
+            con.Open();
+            Result = cmd.ExecuteScalar().ToString();
+            con.Close();
+            return Result;
+        }
+
+        public DataSet GetState(int StateID,int IsActive)
+        {
+            DataSet dsState = new DataSet();
+
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("GetState_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@StateID", StateID);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+            con.Open();
+
+            SqlDataAdapter daGetCountryData = new SqlDataAdapter(cmd);
+            dsState = new DataSet();
+            daGetCountryData.Fill(dsState);
+            con.Close();
+            return dsState;
+        }
+
+        public DataSet BindState(int CountryID)
+        {
+            DataSet dsState = new DataSet();
+
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("BindState_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@CountryID", CountryID);
+
+            con.Open();
+            SqlDataAdapter daGetCountryData = new SqlDataAdapter(cmd);
+            dsState = new DataSet();
+            daGetCountryData.Fill(dsState);
+            con.Close();
+            return dsState;
+
+        }
+    }
+}
