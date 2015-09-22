ef44b24e02b485dd1dc167f629529b679c7ff3ad
ttss2272 <admin@teratech-solutions.com>
2015-09-22 18:41:39 +0530

    PriTesh

---
+++ src/MedicalShopWeb/DataLayer/DLMedicalShop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DLMedicalShop
+    {
+        SqlConnection con = new SqlConnection();
+        DBConnection conn = new DBConnection();
+
+        public DataSet GetShopType()
+        {
+            DataSet dsShopType = new DataSet();
+
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("GetsShopType_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+
+            SqlDataAdapter daGetCountryData = new SqlDataAdapter(cmd);
+            dsShopType = new DataSet();
+            daGetCountryData.Fill(dsShopType);
+            con.Close();
+            return dsShopType;
+ 
+        }
+    }
+}
