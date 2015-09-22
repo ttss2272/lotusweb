4587ea718e6a3760385f80cb06dd5d34d78777d2
ttss2272 <admin@teratech-solutions.com>
2015-09-22 18:40:32 +0530

    PriTesh

---
+++ src/MedicalShopWeb/BusinessLayer/BLMedicalShop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BLMedicalShop
+    {
+        DLMedicalShop objMedicalShop = new DLMedicalShop();
+
+        public DataSet GetShopType()
+        {
+            DataSet dsShopType = objMedicalShop.GetShopType();
+            return dsShopType;
+        }
+    }
+}
