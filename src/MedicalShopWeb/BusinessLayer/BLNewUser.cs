ee2b9eed9372969c2befaadcec412e164d957315
ttss2272 <admin@teratech-solutions.com>
2015-09-23 20:21:30 +0530

    PriTesh

---
+++ src/MedicalShopWeb/BusinessLayer/BLNewUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BLNewUser
+    {
+        DLNewUser objNewUser = new DLNewUser();
+        public DataSet BindUserType()
+        {
+            DataSet dsUserType = objNewUser.BindUserType();
+            return dsUserType;
+        }
+
+        public string CheckLoginName(string LoginName)
+        {
+            string Result = objNewUser.CheckLoginName(LoginName);
+            return Result;
+        }
+
+         public string SaveUser(int UserID, string UserName, int UserTypeID, string EmailID, string ContactNo, int CityID, string Area, string Address, int WarehouseID, string LoginName, string Password, int UpdatedByUserID, int IsActive)
+        {
+            string Result = objNewUser.SaveUser(UserID, UserName, UserTypeID, EmailID, ContactNo, CityID, Area, Address, WarehouseID, LoginName, Password, UpdatedByUserID, IsActive);
+            return Result;
+            }
+    }
+}
---
+++ src/MedicalShopWeb/BusinessLayer/BLWarehouse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BLWarehouse
+    {
+        DLWarehouse objWarehouse = new DLWarehouse();
+         public string SaveWarehosue(int WarehouseID,string WarehouseName,string Location,int UpdatedByUserID,int IsActive)
+        {
+            string Result = objWarehouse.SaveWarehosue(WarehouseID, WarehouseName, Location, UpdatedByUserID, IsActive);
+            return Result;
+        }
+
+         public DataSet GetWarehouse(int WarehouseID,int IsActive)
+         {
+             DataSet dsWarehouse = objWarehouse.GetWarehouse(WarehouseID,IsActive);
+             return dsWarehouse;
+         }
+
+         public DataSet BindWarehouse(int WarehouseID, int IsActive)
+         {
+             DataSet dsWarehouse = objWarehouse.BindWarehouse(WarehouseID,IsActive);
+             return dsWarehouse;
+         }
+    }
+}
