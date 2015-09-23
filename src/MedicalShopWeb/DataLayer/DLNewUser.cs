6736102085e1032003ecd66827377622a9829a39
ttss2272 <admin@teratech-solutions.com>
2015-09-23 20:21:56 +0530

    PriTesh

---
+++ src/MedicalShopWeb/DataLayer/DLNewUser.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DLNewUser
+    {
+        SqlConnection con = new SqlConnection();
+        DBConnection conn = new DBConnection();
+
+        public DataSet BindUserType()
+        {
+            DataSet dsUserType = new DataSet();
+
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("BindUserType_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            
+            
+            con.Open();
+
+            SqlDataAdapter daGetUserTypeData = new SqlDataAdapter(cmd);
+            dsUserType = new DataSet();
+            daGetUserTypeData.Fill(dsUserType);
+            con.Close();
+            return dsUserType;
+ 
+        }
+
+        public string CheckLoginName(string LoginName)
+        {
+            string result = null;
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("CheckLoginName_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@LoginName", LoginName);
+            con.Open();
+            result = cmd.ExecuteScalar().ToString();
+            con.Close();
+            return result;
+
+        }
+
+        public string SaveUser(int UserID, string UserName, int UserTypeID, string EmailID, string ContactNo, int CityID, string Area, string Address, int WarehouseID, string LoginName, string Password, int UpdatedByUserID, int IsActive)
+        {
+            string Result = null;
+
+            con = conn.GetConnection();
+
+            SqlCommand cmd = new SqlCommand("SaveUser_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@UserID", UserID);
+            cmd.Parameters.AddWithValue("@UserName", UserName);
+            cmd.Parameters.AddWithValue("@LoginName", LoginName);
+            cmd.Parameters.AddWithValue("@Password", Password);
+            cmd.Parameters.AddWithValue("@ContactNo", ContactNo);
+            cmd.Parameters.AddWithValue("@Address", Address);
+            cmd.Parameters.AddWithValue("@WarehouseID", WarehouseID);
+            cmd.Parameters.AddWithValue("@EmailAddress", EmailID);
+            cmd.Parameters.AddWithValue("@UserTypeID", UserTypeID);
+            cmd.Parameters.AddWithValue("@UpdatedByUserID", UpdatedByUserID);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+            cmd.Parameters.AddWithValue("@CityID", CityID);
+            cmd.Parameters.AddWithValue("@Area", Area);
+            
+            
+            
+
+            con.Open();
+            Result = cmd.ExecuteScalar().ToString();
+            con.Close();
+            return Result;
+
+        }
+    }
+}
---
+++ src/MedicalShopWeb/DataLayer/DLWarehouse.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DLWarehouse
+    {
+        SqlConnection con = new SqlConnection();
+        DBConnection conn = new DBConnection();
+
+        public string SaveWarehosue(int WarehouseID,string WarehouseName,string Location,int UpdatedByUserID,int IsActive)
+        {
+            string Result = null;
+            con = conn.GetConnection();
+
+            SqlCommand cmd = new SqlCommand("SaveWarehouse_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@WarehouseID", WarehouseID);
+            cmd.Parameters.AddWithValue("@WarehouseName", WarehouseName);
+            cmd.Parameters.AddWithValue("@Location", Location);
+            cmd.Parameters.AddWithValue("@UpdatedByUserID", UpdatedByUserID);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+
+            con.Open();
+            Result = cmd.ExecuteScalar().ToString();
+            con.Close();
+
+            return Result;
+            
+        }
+
+        public DataSet GetWarehouse(int WarehouseID,int IsActive)
+        {
+            DataSet dsWarehouse = new DataSet();
+
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("GetWarehouse_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@WarehouseID", WarehouseID);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+
+            con.Open();
+
+            SqlDataAdapter daGetWarehouseData = new SqlDataAdapter(cmd);
+            dsWarehouse = new DataSet();
+            daGetWarehouseData.Fill(dsWarehouse);
+            con.Close();
+            return dsWarehouse;
+            
+ 
+        }
+
+        public DataSet BindWarehouse(int WarehouseID, int IsActive)
+        {
+            DataSet dsWarehouse = new DataSet();
+
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("BindWarehouse_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@WarehouseID", WarehouseID);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+
+            con.Open();
+
+            SqlDataAdapter daGetWarehouseData = new SqlDataAdapter(cmd);
+            dsWarehouse = new DataSet();
+            daGetWarehouseData.Fill(dsWarehouse);
+            con.Close();
+            return dsWarehouse;
+        }
+    }
+}
