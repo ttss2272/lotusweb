35bdce6dd40e92af680aff1c8189072cb04e084d
ttss2272 <admin@teratech-solutions.com>
2015-09-24 14:04:30 +0530

    PriTesh

---
+++ src/MedicalShopWeb/MedicalShopWeb/Admin/ViewUser.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MedicalShopWeb.Admin
+{
+    public partial class ViewUser : System.Web.UI.Page
+    {
+        #region-----------------------Page_Load-------------------------------------
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = ex.Message.ToString();
+            }
+        }
+        #endregion
+
+        #region--------------------------------btnSearch_Click---------------------------
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = ex.Message.ToString();
+            }
+        }
+        #endregion
+
+        #region---------------------------------btnClose_Click-----------------------
+        protected void btnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = ex.Message.ToString();
+            }
+        }
+        #endregion
+
+        #region-----------------------------BindUser()-------------------------------------
+        private void BindUser()
+        { 
+        }
+        #endregion
+
+        #region-------------------------------------BindGridView()-------------------------
+            private void Bindgridview()
+            {
+            }
+        #endregion
+    }
+}
