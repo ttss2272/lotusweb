8c74c597f90d69b0c6071a7a01e56dab00771efd
ttss2272 <admin@teratech-solutions.com>
2015-09-24 14:03:44 +0530

    PriTesh

---
+++ src/MedicalShopWeb/MedicalShopWeb/Admin/ViewMedicalShop.aspx
@@ -0,0 +1,125 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/MasterPage.Master" AutoEventWireup="true" CodeBehind="ViewMedicalShop.aspx.cs" Inherits="MedicalShopWeb.Admin.ViewMedicalShop" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="CntPlcLeft" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="CntPlcCenter" runat="server">
+<asp:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server"></asp:ToolkitScriptManager>
+<div class="form">
+    <div class="row" align="center">
+        <div class="col-md-12">
+            <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+        </div>
+    </div>
+
+  <div class="form-group">
+      <div class="panel panel-default" align="center">
+       <div class="panel-header" align="left">
+    <div class="col-xs-12">
+        <h3><asp:Label ID="lblPageHeading" runat="server" Text="Medical Shop" class="control-label"></asp:Label></h3>
+    </div>
+    </div>
+   
+
+        <div class="panel-body">
+            <div class="col-md-4"></div>
+     <div class="col-md-4">  
+     <asp:UpdatePanel ID="ddlUpdatePanel" runat="server">
+     <ContentTemplate>
+     <!--Start Of First Row-->
+     <div class="row form-group">
+        <div class="col-sm-4">
+    
+            <asp:Label ID="lblShopType" runat="server" Text="Shop Type" class="control-label">
+            </asp:Label>
+            
+            </div>
+        <div class="col-sm-8">
+            <asp:DropDownList ID="ddlShopType" runat="server"  class="form-control" 
+                AutoPostBack="True" onselectedindexchanged="ddlShopType_SelectedIndexChanged">
+            </asp:DropDownList>
+        </div>
+     </div>
+     <!--End Of First Row-->
+    <!--Start Second Row-->
+     <div class="row form-group">
+        <div class="col-sm-4">
+    
+            <asp:Label ID="lblMedicalShop" runat="server" Text="Medical Name" class="control-label">
+            </asp:Label>
+            
+            </div>
+        <div class="col-sm-8">
+            <asp:DropDownList ID="ddlMedicalShop" runat="server"  class="form-control">
+            </asp:DropDownList>
+        </div>
+     </div> 
+     <!--End Of Second Row-->
+     </ContentTemplate>
+     </asp:UpdatePanel>
+     <!--Start Third Row-->
+     <div class="row form-group">
+        <div class="col-md-12">
+            <asp:Button ID="btnSearch" runat="server" Text="Search" 
+                class=" btn btn-primary" onclick="btnSearch_Click" />
+            <asp:Button ID="btnClose" runat="server" Text="Close"  class=" btn btn-default" 
+                onclick="btnClose_Click"  />
+
+        </div>
+     </div>
+     <!--End of Third Row-->
+     
+     </div>
+        
+     <div class="col-md-4"></div> 
+        </div>
+     <div class="panel-footer" align="left">
+       <asp:Label ID="lblRquirdInfo" runat="server" Text="*" ForeColor="Red"></asp:Label>
+       <asp:Label ID="lblInfo" runat="server" Text="Madnatory Fields" ></asp:Label>
+   </div>
+  
+   <!--Start Grid View-->
+   <asp:UpdatePanel runat ="server" ID="GridUpdate"><ContentTemplate>
+   <div class="panel-body">
+     <div class="row">
+        <asp:GridView ID="grvMedicalShop" runat="server" AutoGenerateColumns="false" class="table" HeaderStyle-BackColor="#4596f1"
+         HeaderStyle-ForeColor="White"  BorderColor="White"  HeaderStyle-BorderColor="#4596f1" AllowPaging="true" onpageindexchanging="grvMedicalShop_PageIndexChanging">
+
+         <Columns>
+            <asp:TemplateField HeaderText="Sr.No" HeaderStyle-HorizontalAlign="Center">
+                <ItemTemplate><%#Container.DataItemIndex+1 %></ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Shop Name" HeaderStyle-HorizontalAlign="Center">
+                <ItemTemplate><%#Eval("")%></ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Owner Name" HeaderStyle-HorizontalAlign="Center">
+                <ItemTemplate><%#Eval("")%></ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Contact No" HeaderStyle-HorizontalAlign="Center">
+                <ItemTemplate><%#Eval("")%></ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Shop Type" HeaderStyle-HorizontalAlign="Center">
+                <ItemTemplate><%#Eval("")%></ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Area" HeaderStyle-HorizontalAlign="Center">
+                <ItemTemplate><%#Eval("")%></ItemTemplate>
+            </asp:TemplateField>
+            <%--<asp:TemplateField HeaderText="Edit" HeaderStyle-HorizontalAlign="Center">
+                    <ItemTemplate >
+                             <asp:HyperLink ID="HyperLinkEdit" runat="server" Text="Edit" NavigateUrl='<%#String.Format("../Admin/Country.aspx?CountryId={0}", DataBinder.Eval(Container.DataItem,"CountryID") )%>'>
+                              </asp:HyperLink>
+                    </ItemTemplate>
+             </asp:TemplateField>--%>
+         </Columns>
+        </asp:GridView>
+     </div>
+    </div>
+    </ContentTemplate></asp:UpdatePanel>
+     <!--End Grid View-->
+      </div>
+  </div>
+</div>
+
+     
+</asp:Content>
---
+++ src/MedicalShopWeb/MedicalShopWeb/Admin/ViewMedicalShop.aspx.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using BusinessLayer;
+
+namespace MedicalShopWeb.Admin
+{
+    public partial class ViewMedicalShop : System.Web.UI.Page
+    {
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  Declare Variales
+        */
+        #region-------------------------------Declare Variables-------------------------
+        int MedicalShopID;
+
+        BLMedicalShop objMedicalShop = new BLMedicalShop();
+        #endregion
+
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  Page_Load
+        */
+        #region-----------------------------Page_Load---------------------------
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!IsPostBack)
+                {
+                    BindShopType();
+                    BindMedicalShop1();
+                    BindGridView();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = ex.Message.ToString();
+                
+            }
+
+        }
+        #endregion
+
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  BindMedicalShop() NOt used In this Form
+        */
+        #region-------------------------------BindMedicalShop()-----------------------
+        private void BindMedicalShop1()
+        {
+            DataSet dsMedicalShop = objMedicalShop.BindMedicalShop(0);
+
+            if (dsMedicalShop.Tables.Count != 0)
+            {
+                if (dsMedicalShop.Tables[0].Rows.Count != 0)
+                {
+                    ddlMedicalShop.DataTextField = "ShopName";
+                    ddlMedicalShop.DataValueField = "MedicalShopID";
+                    ddlMedicalShop.DataSource = dsMedicalShop;
+                    ddlMedicalShop.DataBind();
+                }
+                else
+                {
+                    ddlMedicalShop.DataSource = null;
+                    ddlMedicalShop.DataBind();
+                }
+                ddlMedicalShop.Items.Insert(0, new ListItem("Select All", "0"));
+            }
+        }
+        #endregion
+
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  Bind GridView
+        */
+        #region-------------------------------BindGridView()--------------------------------
+        private void BindGridView()
+        {
+            DataSet dsMedicalShop = objMedicalShop.GetMedicalShopOnShopType(Convert.ToInt32(ddlMedicalShop.SelectedValue), Convert.ToInt32(ddlShopType.SelectedValue));
+
+            if (dsMedicalShop.Tables.Count != 0)
+            {
+                if (dsMedicalShop.Tables[0].Rows.Count != 0)
+                {
+                    grvMedicalShop.DataSource = dsMedicalShop;
+                    grvMedicalShop.DataBind();
+                }
+                else
+                {
+                    grvMedicalShop.DataSource = null;
+                    grvMedicalShop.DataBind();
+                    
+                }
+            }
+        }
+        #endregion
+
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  Bind SHopType()
+        */
+        #region----------------------------------BindShopType()-------------------------------
+        private void BindShopType()
+        {
+            DataSet dsShopType = objMedicalShop.GetShopType();
+            ddlShopType.Items.Clear();
+            if (dsShopType.Tables.Count > 0)
+            {
+                if (dsShopType.Tables[0].Rows.Count > 0)
+                {
+                    ddlShopType.DataTextField = "ShopTypeName";
+                    ddlShopType.DataValueField = "ShopTypeID";
+                    ddlShopType.DataSource = dsShopType;
+                    ddlShopType.DataBind();
+                }
+                ddlShopType.Items.Insert(0, new ListItem("Select All", "0"));
+            }
+        }
+        #endregion
+
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  ShopType_SelectedIndexChanged
+        */
+        #region-------------------------ddlShopType_SelectedIndexchanged------------------------------
+        protected void ddlShopType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                BindMedicalShop();
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = ex.Message.ToString();
+            }
+        }
+        #endregion
+
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  Bind Medical Shop on Shoptype for dropdown
+        */
+        #region---------------------------Bind Medical shop on Shoptype--------------------------------
+        private void BindMedicalShop()
+        {
+            DataSet dsMedicalShop = objMedicalShop.BindMedicalShopOnShopType(Convert.ToInt32(ddlShopType.SelectedValue));
+            ddlMedicalShop.Items.Clear();
+            if (dsMedicalShop.Tables.Count != 0)
+            {
+                if (dsMedicalShop.Tables[0].Rows.Count != 0)
+                {
+                    ddlMedicalShop.DataTextField = "ShopName";
+                    ddlMedicalShop.DataValueField = "MedicalShopID";
+                    ddlMedicalShop.DataSource = dsMedicalShop;
+                    ddlMedicalShop.DataBind();
+                }
+                else
+                {
+                    ddlMedicalShop.DataSource = null;
+                    ddlMedicalShop.DataBind();
+                }
+                ddlMedicalShop.Items.Insert(0, new ListItem("Select All", "0"));
+            }
+        }
+        #endregion
+
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  PageIndexChangeing
+        */
+        #region--------------------------------grvMedicalShop_PageIndexChangeing--------------------------
+
+        protected void grvMedicalShop_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            try
+            {
+                grvMedicalShop.PageIndex = e.NewPageIndex;
+                BindGridView();
+                grvMedicalShop.Focus();
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = ex.Message.ToString();
+            }
+
+        }
+
+        #endregion
+
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  btnSearch_Click
+        */
+        #region---------------------------btnSearch_Click---------------------------------------
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BindGridView();
+
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = ex.Message.ToString();
+            }
+        }
+        #endregion
+
+        /*
+        * Created By :- PriTesh D. Sortee
+        * Created Date:- 24 Sept 2015
+        * Purpose :-  BtnClose_Click
+        */
+        #region--------------------------------btnClose_Click-----------------------------------
+        protected void btnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = ex.Message.ToString();
+            }
+        }
+        #endregion
+    }
+}
