af7df8cc038f1b839cb5ddba6a9918a62b0dc0cd
ttss2272 <admin@teratech-solutions.com>
2015-09-25 17:28:58 +0530

    PriTesh
    Login Code

---
+++ src/MedicalShopWeb/DataLayer/DLLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DLLogin
+    {
+        SqlConnection con = new SqlConnection();
+        DBConnection conn = new DBConnection();
+
+        public DataSet UserLogin(string LoginName, string Password)
+        {
+            DataSet SessionInfo = null;
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("UserLogin_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@LoginName", LoginName);
+            cmd.Parameters.AddWithValue("@Password", Password);
+
+            con.Open();
+
+            SqlDataAdapter daSessionData = new SqlDataAdapter(cmd);
+            SessionInfo = new DataSet();
+            daSessionData.Fill(SessionInfo);
+
+
+            con.Close();
+            return SessionInfo;
+
+        }
+    }
+}
