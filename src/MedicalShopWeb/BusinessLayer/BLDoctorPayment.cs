104c50c6a39d937e938899421feffcb5f189b695
ttss2272 <admin@teratech-solutions.com>
2015-09-25 16:19:36 +0530

    pravin create save button coding of DoctorPayment

---
+++ src/MedicalShopWeb/BusinessLayer/BLDoctorPayment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BLDoctorPayment
+    {
+        DLDoctorPayment objDoctorPayment = new DLDoctorPayment();
+        public DataSet GetDoctorPay()
+        {
+            DataSet dsDoctorPay = objDoctorPayment.GetDoctorPay();
+            return dsDoctorPay;
+        }
+
+        public string SaveDoctorsPayment(int DoctorPaymentID, int DoctorsID, string PaidAmountDate, double PaymentAmount, string Comment, int UpdatedByUserID, int IsActive, string RecieptNo)
+        {
+            string Result = null;
+            Result = objDoctorPayment.SaveDoctorsPayment( DoctorPaymentID,  DoctorsID,  PaidAmountDate,  PaymentAmount,  Comment,  UpdatedByUserID,  IsActive,  RecieptNo);
+            return Result;
+        }
+    }
+}
