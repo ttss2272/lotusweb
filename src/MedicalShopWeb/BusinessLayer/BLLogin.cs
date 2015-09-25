3c6a0099352697a10a582ede16e0bb47158e422c
ttss2272 <admin@teratech-solutions.com>
2015-09-25 17:29:25 +0530

    PriTesh

---
+++ src/MedicalShopWeb/BusinessLayer/BLLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BLLogin
+    {
+        DLLogin objLogin= new DLLogin();
+        public DataSet UserLogin(string LoginName, string Password)
+        {
+            DataSet SessionInfo = objLogin.UserLogin(LoginName, Password);
+            return SessionInfo;
+        }
+    }
+
+}
