a51cd1b33f23de5003ffa3519edaa32d0eae2d7e
ttss2272 <admin@teratech-solutions.com>
2015-09-26 17:40:38 +0530

    PriTesh

---
+++ src/MedicalShopWeb/BusinessLayer/BLDMPLinking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataLayer;
+namespace BusinessLayer
+{
+    public class BLDMPLinking
+    {
+        DLDMPLinking objDMPLinking = new DLDMPLinking();
+
+        public string SaveDMPLinking(int DMPID, int DoctorID, int MedicalShopID, int ProductID, int PriceTypeID, decimal Price, int UpdatedByUserID, int IsActive)
+        {
+            string Result = objDMPLinking.SaveDMPLinking(DMPID, DoctorID, MedicalShopID, ProductID, PriceTypeID, Price, UpdatedByUserID, IsActive);
+            return Result;
+        }
+
+        public DataSet GetAllDMPDetail(int DoctorID)
+        {
+            DataSet dsDMPDetail = objDMPLinking.GetAllDMPDetail(DoctorID);
+            return dsDMPDetail;
+        }
+
+        public DataSet BindDMPDetail(int DMPID)
+        {
+            DataSet dsDMPDetail = objDMPLinking.BindDMPDetail(DMPID);
+            return dsDMPDetail;
+        }
+
+        public DataSet GetDmpPrice(int DoctorID, int MedicalShopID, int ProductID)
+        {
+            DataSet dsDMPPrice = objDMPLinking.GetDmpPrice(DoctorID, MedicalShopID, ProductID);
+            return dsDMPPrice;
+        }
+    }
+}
