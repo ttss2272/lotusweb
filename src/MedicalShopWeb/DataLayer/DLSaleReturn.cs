d60299303c25c4983abbf2f41b9d3674da95326b
ttss2272 <admin@teratech-solutions.com>
2015-09-28 13:18:13 +0530

    PriTesh

---
+++ src/MedicalShopWeb/DataLayer/DLSaleReturn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DLSaleReturn
+    {
+        SqlConnection con = new SqlConnection();
+        DBConnection conn = new DBConnection();
+
+    }
+}
