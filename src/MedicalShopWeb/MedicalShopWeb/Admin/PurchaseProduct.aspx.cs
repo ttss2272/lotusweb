e5ceb408bc9aa92c13e8c65e0ce3515c4b275f12
ttss2272 <admin@teratech-solutions.com>
2015-09-28 16:10:10 +0530

    PriTesh
    bind Drop down

--- src/MedicalShopWeb/MedicalShopWeb/Admin/PurchaseProduct.aspx.cs
+++ src/MedicalShopWeb/MedicalShopWeb/Admin/PurchaseProduct.aspx.cs
@@ -19,6 +19,12 @@ namespace MedicalShopWeb.Admin
         #region-----------------------------------Declare Variable Globally-------------------------
         BLWarehouse objWarehouse = new BLWarehouse();
         BLProductType objProductType = new BLProductType();
+        BLProducts objProducts = new BLProducts();
+        BLSupplier objSupplier = new BLSupplier();
+
+        int PurchaseProductID,WarehouseID,SupplierID,ProductTypID,ProductID,IsActive;
+            string PurchaseDate,ReceiptNo,ModeOfPayment,BatchNo,ExpiryDate;
+            decimal PurchasePrice, SellingPrice, PurchaseQuantity;
         #endregion
         /*
          * Created By :- PriTesh D. Sortee
@@ -30,7 +36,12 @@ namespace MedicalShopWeb.Admin
         {
             try
             {
-
+                if (!IsPostBack)
+                {
+                    BindWarehouse();
+                    BindSupplier();
+                    BindProductType();
+                }
             }
             catch (Exception ex)
             {
@@ -153,6 +164,8 @@ namespace MedicalShopWeb.Admin
                     ddlProductType.DataSource = null;
                     ddlProductType.DataBind();
                 }
+                ddlProductType.Items.Insert(0, new ListItem("Select Product Type", "-1"));
+                ddlProduct.Items.Insert(0, new ListItem("Select Product", "-1"));
             }
         }
         #endregion
@@ -167,7 +180,7 @@ namespace MedicalShopWeb.Admin
         {
             try
             {
-                
+                BindProduct();
             }
             catch (Exception ex)
             {
@@ -185,7 +198,98 @@ namespace MedicalShopWeb.Admin
         #region------------------------------------------BindProduct()---------------------------------------
         private void BindProduct()
         {
+            
+            DataSet dsProduct=objProducts.BindProductOnProductType(Convert.ToInt32(ddlProductType.SelectedValue));
+            ddlProduct.Items.Clear();
+            if(dsProduct.Tables.Count!=0)
+            {
+                if(dsProduct.Tables[0].Rows.Count!=0)
+                {
+                    ddlProduct.DataTextField="ProductName";
+                    ddlProduct.DataValueField="ProductID";
+                    ddlProduct.DataSource=dsProduct;
+                    ddlProduct.DataBind();
+                }
+                else
+                {
+                    ddlProduct.DataSource=null;
+                    ddlProduct.DataBind();
+                }
+                ddlProduct.Items.Insert(0,new ListItem("Select Product","-1"));
+            }
  
+        }
+        #endregion
+
+        /*
+         * Created By :- PriTesh D. Sortee
+         * Created Date:- 28 sept 2015
+         * purpose :- BindSupplier()
+         */
+        #region---------------------------------------BindSupplier()----------------------------------------
+        private void BindSupplier()
+        {
+            DataSet dsSupplier = objSupplier.BindSupplier(0);
+            ddlSupplier.Items.Clear();
+
+            if (dsSupplier.Tables.Count != 0)
+            {
+                if (dsSupplier.Tables[0].Rows.Count != 0)
+                {
+                    ddlSupplier.DataTextField = "CompanyName";
+                    ddlSupplier.DataValueField = "SupplierID";
+                    ddlSupplier.DataSource = dsSupplier;
+                    ddlSupplier.DataBind();
+                }
+                else
+                {
+                    ddlSupplier.DataSource = null;
+                    ddlSupplier.DataBind();
+                }
+                ddlSupplier.Items.Insert(0, new ListItem("Select Supplier", "-1"));
+            }
+        }
+        #endregion
+
+        /*
+         * Created By :- PriTesh D. Sortee
+         * Created Date:- 28 sept 2015
+         * purpose :- SetAddParameters()
+         */
+        #region------------------------------------------SetAddParameters()-----------------------------------
+        private void SetAddParameters()
+        {
+            WarehouseID = Convert.ToInt32(ddlWarehouse.SelectedValue);
+            PurchaseDate = txtPurchaseDate.Text;
+            SupplierID = Convert.ToInt32(ddlSupplier.SelectedValue);
+            ReceiptNo = txtReceiptNo.Text;
+            ModeOfPayment = txtModeOfPayment.Text;
+            
+
+ 
+        }
+        #endregion
+
+        /*
+         * Created By :- PriTesh D. Sortee
+         * Created Date:- 28 sept 2015
+         * purpose :- SetSaveParameters()
+         */
+        #region------------------------------------------SetSaveParameters()-----------------------------------
+        private void SetSaveParameters()
+        {
+        }
+        #endregion
+
+        #region----------------------------------------SetProductDetail()-------------------------------------
+        private void SetProductDetail()
+        {
+            ProductID = Convert.ToInt32(ddlProduct.SelectedValue);
+            PurchaseQuantity = Convert.ToDecimal(txtQuantity.Text);
+            PurchasePrice = Convert.ToDecimal(txtPurchasePrice.Text);
+            SellingPrice = Convert.ToDecimal(txtSellingPrice.Text);
+            BatchNo = txtBatchNo.Text;
+            ExpiryDate = txtExpiryDate.Text;
         }
         #endregion
     }
