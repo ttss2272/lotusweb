13f40a13b113b1b37acc05ad96c3885f1b3b4640
ttss2272 <admin@teratech-solutions.com>
2015-09-29 18:49:07 +0530

    PriTesh
    Modify

--- src/MedicalShopWeb/DataLayer/DLProducts.cs
+++ src/MedicalShopWeb/DataLayer/DLProducts.cs
@@ -47,7 +47,25 @@ namespace DataLayer
             daGetProductData.Fill(dsProduct);
             con.Close();
             return dsProduct;
-        }               
+        }        
+
+
+        //public DataSet GetProductType()bbbb
+        //{
+        //    DataSet dsProductType = new DataSet();
+
+        //    con = conn.GetConnection();
+        //    SqlCommand cmd = new SqlCommand("ProductType_USP", con);
+        //    cmd.CommandType = CommandType.StoredProcedure;
+
+        //    con.Open();
+
+        //    SqlDataAdapter daGetProductData = new SqlDataAdapter(cmd);
+        //    dsProductType = new DataSet();
+        //    daGetProductData.Fill(dsProductType);
+        //    con.Close();
+        //    return dsProductType;
+        //}
 
         public DataSet BindProductOnProductType(int ProductTypeID)
         {
@@ -87,22 +105,20 @@ namespace DataLayer
             return dsProduct;
         }
 
-        public DataSet GetProductDetail(int ProductID, int ProductTypeID)
+        public DataSet GetProductPrice(int ProductID)
         {
             DataSet dsProduct = new DataSet();
 
             con = conn.GetConnection();
-            SqlCommand cmd = new SqlCommand("GetProductAllDetail_USP", con);
+            SqlCommand cmd = new SqlCommand("GetProductPrice_USP", con);
             cmd.CommandType = CommandType.StoredProcedure;
+
             cmd.Parameters.AddWithValue("@ProductID", ProductID);
-            cmd.Parameters.AddWithValue("@ProductTypeID", ProductTypeID);
-
-
             con.Open();
 
-            SqlDataAdapter daGetProductData = new SqlDataAdapter(cmd);
+            SqlDataAdapter daGetProductPriceData = new SqlDataAdapter(cmd);
             dsProduct = new DataSet();
-            daGetProductData.Fill(dsProduct);
+            daGetProductPriceData.Fill(dsProduct);
             con.Close();
             return dsProduct;
         }
