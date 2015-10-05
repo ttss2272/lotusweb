50afb48ad25108f58a10de4e5a6544790970c095
ttss2272 <admin@teratech-solutions.com>
2015-10-05 13:31:26 +0530

    PRAVIN changes in BLSaleReturn

--- src/MedicalShopWeb/BusinessLayer/BLSaleReturn.cs
+++ src/MedicalShopWeb/BusinessLayer/BLSaleReturn.cs
@@ -10,5 +10,17 @@ namespace BusinessLayer
     public class BLSaleReturn
     {
         DLSaleReturn objSaleReturn = new DLSaleReturn();
+
+        public string SaveReturnSalesRecord(int ProductID, double Quantity, int SalesReturnID, string Reason, double Rate)
+        {
+            string result = objSaleReturn.SaveReturnSalesRecord(ProductID, Quantity, SalesReturnID, Reason, Rate);
+            return result;
+        }
+
+        public string saveReturnSales(int SalesReturnID, string ReturnInvoiceNo, int MedicalShopID, string ReturnDate, int UpdatedByUserID, int WarehouseID, string Comment)
+        {
+            string result = objSaleReturn.saveReturnSales(SalesReturnID, ReturnInvoiceNo, MedicalShopID, ReturnDate, UpdatedByUserID, WarehouseID, Comment);
+            return result;
+        }       
     }
 }
