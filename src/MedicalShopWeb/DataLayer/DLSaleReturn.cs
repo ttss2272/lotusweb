fe94b9b4879e786cc54c301e1d534f5d710c2092
ttss2272 <admin@teratech-solutions.com>
2015-10-05 13:30:50 +0530

    pravin changes in DLSaleReturn

--- src/MedicalShopWeb/DataLayer/DLSaleReturn.cs
+++ src/MedicalShopWeb/DataLayer/DLSaleReturn.cs
@@ -12,5 +12,45 @@ namespace DataLayer
         SqlConnection con = new SqlConnection();
         DBConnection conn = new DBConnection();
 
+
+        public string saveReturnSales(int SalesReturnID, string ReturnInvoiceNo, int MedicalShopID, string ReturnDate, int UpdatedByUserID, int WarehouseID, string Comment)
+        {
+            string result = null;
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("Usp_SaveReturnSaleTransaction", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("SalesReturnID", SalesReturnID);
+            cmd.Parameters.AddWithValue("ReturnInvoiceNo", ReturnInvoiceNo);
+            cmd.Parameters.AddWithValue("MedicalShopID", MedicalShopID);
+            cmd.Parameters.AddWithValue("ReturnDate", ReturnDate);
+            cmd.Parameters.AddWithValue("UpdatedByUserID", UpdatedByUserID);
+            cmd.Parameters.AddWithValue("WarehouseID", WarehouseID);
+            cmd.Parameters.AddWithValue("Comment", Comment);
+            con.Open();
+            result = cmd.ExecuteScalar().ToString();
+            con.Close();
+            return result;
+        }
+
+        public string SaveReturnSalesRecord(int ProductID, double Quantity, int SalesReturnID, string Reason, double Rate)
+        {
+            string result = null;
+            con = conn.GetConnection();
+            SqlCommand cmd = new SqlCommand("Usp_SaveTempSaleReturn", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@ProductID", ProductID);
+            cmd.Parameters.AddWithValue("@Quantity", Quantity);
+            cmd.Parameters.AddWithValue("@SalesReturnID", SalesReturnID);
+            cmd.Parameters.AddWithValue("@Reason", Reason);
+            cmd.Parameters.AddWithValue("@Rate", Rate);
+
+            con.Open();
+            result = cmd.ExecuteNonQuery().ToString();
+            con.Close();
+            return result;
+        }
+
+      
     }
 }
