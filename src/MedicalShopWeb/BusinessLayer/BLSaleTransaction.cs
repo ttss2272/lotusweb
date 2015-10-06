451e12f1c4ab24f237a950639c9b3f3bea5fc8b2
ttss2272 <admin@teratech-solutions.com>
2015-10-06 13:40:06 +0530

    Sameer

--- src/MedicalShopWeb/BusinessLayer/BLSaleTransaction.cs
+++ src/MedicalShopWeb/BusinessLayer/BLSaleTransaction.cs
@@ -10,15 +10,24 @@ namespace BusinessLayer
     public class BLSaleTransaction
     {
         DLSaleTransaction objSaleTransaction = new DLSaleTransaction();
-        public string SaveSaleProduct(int SaleTransactionID, string SaleTransactionNo, int WarehouseID, int MedicalID, string SellingDate, int UpdatedByUserID,decimal DiscountAmt,decimal BalAmt)
+             public string SaveSaleProduct(int SaleTransactionID, string SaleTransactionNo, int WarehouseID, int MedicalID, string SellingDate, int UpdatedByUserID,decimal DiscountAmt,decimal BalAmt)
             {
                 string result = objSaleTransaction.SaveSaleTransation(SaleTransactionID, SaleTransactionNo, WarehouseID, MedicalID, SellingDate, UpdatedByUserID, DiscountAmt, BalAmt);
-            return result;
+               return result;
              }
-
-            public string SaveTempSaleDetail(int SaleProductID, int ProductID, decimal Quantity, decimal SalePrice)
+             public string SaveSaleTransactionDetails(int SaleTransactionDetailID, int SaleTansactionID, int ProductID, decimal Quntity, decimal SellingPrice, int IsActive)
+             {
+                 string result = objSaleTransaction.SaveSaleTransactionDetails(SaleTransactionDetailID, SaleTansactionID, ProductID, Quntity, SellingPrice, IsActive);
+                 return result;
+             }
+             public int AddMedicalStock(string DateOfStock, double CurrentStock, int MedicalShopID, int ProductID)
+             {
+                 int result = objSaleTransaction.AddMedicalStock(DateOfStock,CurrentStock,MedicalShopID,ProductID);
+                 return result;
+             }
+            public string SaveTempSaleDetail(int SaleTransactionID, int ProductID, decimal Quantity, decimal SalePrice,decimal DiscountAmt)
             {
-                string result = objSaleTransaction.SaveTempSaleDetail(SaleProductID, ProductID, Quantity, SalePrice);
+                string result = objSaleTransaction.SaveTempSaleDetail(SaleTransactionID, ProductID, Quantity, SalePrice, DiscountAmt);
                 return result;
             }
 
@@ -26,6 +35,11 @@ namespace BusinessLayer
             {
                 DataSet dsSaleProductDetails = objSaleTransaction.GetTempSaleDetail(SaleProductID);
                 return dsSaleProductDetails;
+            }
+            public DataSet GetDataForSaleTransDetail(int SaleTransactionID)
+            {
+                DataSet dsProductDetails = objSaleTransaction.GetDataForSaleTransDetail(SaleTransactionID);
+                return dsProductDetails;
             }
     }
 }
