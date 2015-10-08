5fa90ce7168347370d743baf1d26b5c90efc8f7f
ttss2272 <admin@teratech-solutions.com>
2015-10-08 18:52:30 +0530

    PriTesh
    Supplier Payment

--- src/MedicalShopWeb/DataLayer/DLSupplierPayment.cs
+++ src/MedicalShopWeb/DataLayer/DLSupplierPayment.cs
@@ -12,18 +12,29 @@ namespace DataLayer
         SqlConnection con = new SqlConnection();
         DBConnection conn = new DBConnection();    
         /* For Bind Purchase Invoice No to DropDown */
-        public DataSet BindPurchaseInvoiceNo()
+        public DataSet BindPurchaseInvoiceNo(int SupplierID)
         {
-            throw new NotImplementedException();
+            DataSet dsInvoiceNo = new DataSet();
+            con = conn.GetConnection();
+
+            SqlCommand cmd = new SqlCommand("GetSupplierInvoiceNo_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@SupplierID", SupplierID);
+            con.Open();
+            SqlDataAdapter daPurchaseInvoiceData = new SqlDataAdapter(cmd);
+            dsInvoiceNo = new DataSet();
+            daPurchaseInvoiceData.Fill(dsInvoiceNo);
+            con.Close();
+            return dsInvoiceNo;
         }
         /* For Get Total amt,currant balance and bind to text box */
-        public DataSet GetTotalAmt(int PurchaseInvoiceNo)
+        public DataSet GetTotalAmt(int PurchaseTransactionID)
         {
             DataSet dsTotalAmt = new DataSet();
             con = conn.GetConnection();
-            SqlCommand cmd = new SqlCommand("GetTotal_USP", con);
+            SqlCommand cmd = new SqlCommand("GetInvoiceBalance_USP", con);
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@PurchaseTransactionID", PurchaseInvoiceNo);
+            cmd.Parameters.AddWithValue("@PurchaseTransactionID", PurchaseTransactionID);
             con.Open();
             SqlDataAdapter daGetTotalAmt = new SqlDataAdapter(cmd);
             dsTotalAmt = new DataSet();
@@ -44,12 +55,25 @@ namespace DataLayer
             cmd.Parameters.AddWithValue("@AmountPaidDate", PaymentDate);
             cmd.Parameters.AddWithValue("@UpdatedByUserID", UpdatedByUserID);
             cmd.Parameters.AddWithValue("@PaymentReceiptNo", SupplierPaymentNo);
-            cmd.Parameters.AddWithValue("@BalaneAmount", BalanceAmount);
+            cmd.Parameters.AddWithValue("@BalanceAmount", BalanceAmount);
             cmd.Parameters.AddWithValue("@Comment", Comment);
             con.Open();
             result = cmd.ExecuteScalar().ToString();
             con.Close();
             return result;
+        }
+
+        public DataSet SetSupplierPaymentRecieptNo()
+        {
+            con = conn.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SetSupplierPaymentRecieptNo_USP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sqlDa.Fill(ds);
+            con.Close();
+            return ds;
         }
     }
 }
